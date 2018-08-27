7062dd0399e15fa3ceadb4169b09c57706093dfa
Danijel Sugar <danijel.sugar@outlook.com>
2018-08-27 16:00:55 +0200

    Footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer>
+	<hr>
+	<p>This is my footer</p>
+	<?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
+</footer>
+
+
+	</div><!-- /.container -->
+
+	<?php wp_footer(); ?>
+
+</body>
+</html>
